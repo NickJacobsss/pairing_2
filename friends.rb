88601f61519b013a0f9eca4729d0c1fb16726715
Nick Jacobs <nickjacobsatc@gmail.com>
2022-03-09 18:17:22 -0600

    add good friend

--- friends.rb
+++ friends.rb
@@ -1,6 +1,14 @@
 best_friend = "Candace"
+good_friend = "Nick"
+
 if best_friend == "Candace"
   puts "Hey bestie"
+else
+  puts "Hey not bestie"
+end
+
+if good_friend == "Nick"
+  puts "Hey very good friend"
 else
   puts "Hey not bestie"
 end
